7e7e04264d88d6ed14b32f053c690d0a38126255
23watejona <58493718+23watejona@users.noreply.github.com>
2023-01-10 19:51:06 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,182 @@
+package frc.robot;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.SPI;
+
+
+public final class Constants {
+
+	public static final class ArduinoConstants {
+		public static final int kAddress = 2;
+		public static final double kDistanceP = 0;
+		public static final double kDistanceI = 0;
+		public static final double kDistanceD = 0;
+		public static final int kDistanceSetpoint = 0;
+
+		public static final double kAngleP = 0;
+		public static final double kAngleI = 0;
+		public static final double kAngleD = 0;
+		public static final int kAngleSetpoint = 0;
+
+		public static final int kReadTargetInView = 0;
+		public static final int[] kReadXValue = { 1, 2, 3 };
+		public static final int[] kReadDistance = { 4, 5, 6 };
+
+		public static final int kWriteMainLEDMode = 0;
+		public static final int kWriteMainLEDValue = 1;
+		public static final int kWriteShooterLEDMode = 2;
+		public static final int kWriteShooterLEDValue = 3;
+		public static final int kWriteClimberLEDMode = 2;
+		public static final int kWriteClimberLEDValue = 3;
+
+		public static final class LEDModes {
+			public static final byte kReset = 0;
+			public static final byte kOff = 1;
+			public static final byte kSolid = 2;
+			public static final byte kChasing = 3;
+			public static final byte kTheaterLights = 4;
+			public static final byte kRedGreenGradient = 5;
+			public static final byte kBlueGreenGradient = 6;
+			public static final byte kBackForthTimer = 7;
+		}
+
+		public static final class LEDColors {
+			public static final byte kOff = 0;
+			public static final byte kRed = 1;
+			public static final byte kOrange = 2;
+			public static final byte kYellow = 3;
+			public static final byte kGreen = 4;
+			public static final byte kBlue = 5;
+			public static final byte kPurple = 6;
+			public static final byte kWhite = 7;
+		}
+	}
+
+	public static final class ControllerConstants {
+		public static final int kDriverControllerPort = 0;
+		public static final int kOperatorControllerPort = 1;
+		public static final double kDeadzone = 0.1;
+		public static final double kTriggerDeadzone = .05;
+
+		public static final class Axis {
+			public static final int kLeftX = 0;
+			public static final int kLeftY = 1;
+			public static final int kRightX = 2;
+			public static final int kLeftTrigger = 3;
+			public static final int kRightTrigger = 4;
+			public static final int kRightY = 5;
+		}
+
+		public static final class Button {
+			public static final int kSquare = 1;
+			public static final int kX = 2;
+			public static final int kCircle = 3;
+			public static final int kTriangle = 4;
+			public static final int kLeftBumper = 5;
+			public static final int kRightBumper = 6;
+			public static final int kShare = 9;
+			public static final int kOptions = 10;
+			public static final int kLeftStick = 11;
+			public static final int kRightStick = 12;
+			public static final int kPS = 13;
+			public static final int kTrackpad = 14;
+		}
+
+		public static final class DPad {
+			public static final int kUp = 0;
+			public static final int kRight = 90;
+			public static final int kDown = 180;
+			public static final int kLeft = 270;
+		}
+	}
+
+	public static final class DriveConstants {
+
+        //TODO CHANGE ALL OF THESE
+		public static final int kFrontLeftPort = 3;
+		public static final boolean kFrontLeftInvert = true;
+		public static final int kBackLeftPort=2;
+		public static final boolean kBackLeftOppose = false;
+
+		public static final int kFrontRightPort = 4;
+		public static final boolean kFrontRightInvert = false;
+		public static final int kBackRightPort = 5;
+		public static final boolean kBackRightOppose = false;
+
+		public static final int kSmartCurrentLimit = 55;
+		public static final double kPeakCurrentLimit = 65;
+		public static final int kPeakCurrentDurationMillis = 0;
+		public static final double kP = .14;// 0.198;
+		public static final double kI = 0;
+		public static final double kD = 0;
+		public static final double kIz = 0;
+		public static final double kFF = 0;
+		public static final double kMaxOutput = 1;
+		public static final double kMinOutput = -1;
+		public static final int kSlotID = 0;
+
+		public static final double kFineTurningSpeed = .1;
+
+		// navX stuff
+		public static final SPI.Port kGyroPort = SPI.Port.kMXP;
+		public static final boolean kGyroReversed = true;
+
+        //TODO CHANGE ALL OF THESE
+		public static final double kTurnP = 0.0125; //was 0.005
+		public static final double kTurnI = 0; //was 0.003
+		public static final double kTurnD = 0; //0.0
+		public static final double kTurnTolerance = 0.5;
+		public static final double ksVolts = 0.196;
+		public static final double kvVoltSecondsPerMeter = 2.15;
+		public static final double kaVoltSecondsSquaredPerMeter = .53;
+		// TODO change the trackwidth to match our robot - trackwidth = horizontal
+		// distance between the wheels
+		public static final double kTrackwidthMeters = 0.7815245428457417;
+		public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(
+				kTrackwidthMeters);
+
+		// TODO change
+		public static final double kMaxSpeedMetersPerSecond = 1;
+		public static final double kMaxAccelerationMetersPerSecondSquared = .5;
+		public static final double kMaxRotSpeedMetersPerSecond = 1;
+		public static final double kWheelDiameterMeters = 4;
+		public static final double kGearRatio = 7;
+		public static final double kTurningMultiplier = .45;
+		public static final double kQuickStopThreshold = .2;
+		public static final double kQuickStopAlpha = .1;
+		public static final double kBackupDistance = Units.feetToMeters(2);
+		public static final double kRampRate = .1;//1?
+		public static final double kSpeedLimitFactor = .5;
+		public static final boolean kLeftSensorPhase = true; // TODO these are totally arbitrary right now and need to														// be checked
+		public static final boolean kRightSensorPhase = false;
+		public static final boolean kEnableVoltageComp = true;
+		public static final double kVoltageComp = 12;
+		public static final double kEncoderCounts = 4096;
+		public static final double kEncoderPositionConversionFactor = (1 / DriveConstants.kGearRatio) * Math.PI
+				* DriveConstants.kWheelDiameterMeters;
+		public static final double kEncoderVelocityConversionFactor = (1 / DriveConstants.kGearRatio) * Math.PI
+				* DriveConstants.kWheelDiameterMeters * 60;
+
+	}
+
+
+	public static final class LimelightConstants { // TODO: tune PID loop
+		public static final double kDisP = 0.02;
+		public static final double kDisI = 0;
+		public static final double kDisD = 0;
+		public static final double kTurnP = 0.03;
+		public static final double kTurnI = 0.0000;
+		public static final double kTurnD = 0.0;
+		public static final double kTurnTolerance = 2; // TODO: this is the amount of error that is considered okay
+														// because obviously we can't get perfectly to the setpoint
+		public static final double kDistanceTolerance = 0.5;
+		public static final double kCameraHeight = 22.5; // TODO: get height once mounted
+		public static final double kCameraAngle = 25.453;// 29.8394991; //TODO: get angle once mounted
+		public static final double kTargetHeight = 104; // TODO: get height of target (inches)
+		public static final double kRefreshRate = 0.01111; // matches with the max of 90 frames/second from the
+															// limelight
+		public static final int kRollingAverageSize = 10; // TODO: change, experiment
+	}
+
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,269 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {	
+  
+        private static DriveSubsystem s_subsystem;
+        public static DriveSubsystem get(){return s_subsystem;}
+        private final CANSparkMax m_frontLeft = new CANSparkMax(DriveConstants.kFrontLeftPort, MotorType.kBrushless);
+        private final CANSparkMax m_frontRight = new CANSparkMax(DriveConstants.kFrontRightPort, MotorType.kBrushless);
+        private final CANSparkMax m_backLeft = new CANSparkMax(DriveConstants.kBackLeftPort, MotorType.kBrushless);
+        private final CANSparkMax m_backRight = new CANSparkMax(DriveConstants.kBackRightPort, MotorType.kBrushless);
+
+        private final RelativeEncoder m_leftEncoder = m_frontLeft.getEncoder();
+        private final RelativeEncoder m_rightEncoder = m_frontRight.getEncoder();
+        private final SparkMaxPIDController m_leftPIDController = m_frontLeft.getPIDController();
+        private final SparkMaxPIDController m_rightPIDController = m_frontRight.getPIDController();
+
+        private final AHRS m_gyro = new AHRS(DriveConstants.kGyroPort);
+        //private final PIDController m_turnController = new PIDController(DriveConstants.kTurnP, DriveConstants.kTurnI, DriveConstants.kTurnP);
+
+        private final DifferentialDriveOdometry m_odometry;
+
+        public DriveSubsystem() {
+
+                s_subsystem = this;
+                m_frontLeft.restoreFactoryDefaults();
+                m_frontLeft.setInverted(DriveConstants.kFrontLeftInvert);
+                m_frontLeft.setIdleMode(IdleMode.kBrake);
+                m_frontLeft.enableVoltageCompensation(12);
+                m_frontLeft.setSmartCurrentLimit(DriveConstants.kSmartCurrentLimit);
+                m_frontLeft.setSecondaryCurrentLimit(DriveConstants.kPeakCurrentLimit,
+                                DriveConstants.kPeakCurrentDurationMillis);
+                m_frontLeft.setOpenLoopRampRate(DriveConstants.kRampRate);
+
+                m_backLeft.restoreFactoryDefaults();
+                m_backLeft.setIdleMode(IdleMode.kCoast);
+                m_backLeft.enableVoltageCompensation(12);
+                m_backLeft.setSmartCurrentLimit(DriveConstants.kSmartCurrentLimit);
+                m_backLeft.setSecondaryCurrentLimit(DriveConstants.kPeakCurrentLimit,
+                                DriveConstants.kPeakCurrentDurationMillis);
+                m_backLeft.setOpenLoopRampRate(DriveConstants.kRampRate);
+                m_backLeft.follow(m_frontLeft, DriveConstants.kBackLeftOppose);
+
+                m_frontRight.restoreFactoryDefaults();
+                m_frontRight.setInverted(DriveConstants.kFrontRightInvert);
+                m_frontRight.setIdleMode(IdleMode.kBrake);
+                m_frontRight.enableVoltageCompensation(12);
+                m_frontRight.setSmartCurrentLimit(DriveConstants.kSmartCurrentLimit);
+                m_frontRight.setSecondaryCurrentLimit(DriveConstants.kPeakCurrentLimit,
+                                DriveConstants.kPeakCurrentDurationMillis);
+                m_frontRight.setOpenLoopRampRate(DriveConstants.kRampRate);
+
+                m_backRight.restoreFactoryDefaults();
+                m_backRight.setIdleMode(IdleMode.kCoast);
+                m_backRight.enableVoltageCompensation(12);
+                m_backRight.setSmartCurrentLimit(DriveConstants.kSmartCurrentLimit);
+                m_backRight.setSecondaryCurrentLimit(DriveConstants.kPeakCurrentLimit,
+                                DriveConstants.kPeakCurrentDurationMillis);
+                m_backRight.setOpenLoopRampRate(DriveConstants.kRampRate);
+                m_backRight.follow(m_frontRight, DriveConstants.kBackRightOppose);
+
+                m_leftEncoder.setPositionConversionFactor(
+                                (1 / DriveConstants.kGearRatio) * Math.PI* DriveConstants.kWheelDiameterMeters);
+                m_leftEncoder.setVelocityConversionFactor(
+                                (1 / DriveConstants.kGearRatio) * Math.PI * DriveConstants.kWheelDiameterMeters / 60.0);
+
+                m_rightEncoder.setPositionConversionFactor(
+                                (1 / DriveConstants.kGearRatio) * Math.PI * DriveConstants.kWheelDiameterMeters);
+                m_rightEncoder.setVelocityConversionFactor(
+                                (1 / DriveConstants.kGearRatio) * Math.PI * DriveConstants.kWheelDiameterMeters / 60.0);
+
+                // m_backRight.setControlFramePeriodMs(10);
+
+                m_leftPIDController.setP(DriveConstants.kP);
+                m_leftPIDController.setI(DriveConstants.kI);
+                m_leftPIDController.setIZone(DriveConstants.kIz);
+                m_leftPIDController.setD(DriveConstants.kD);
+                m_leftPIDController.setFF(DriveConstants.kFF);
+                m_leftPIDController.setOutputRange(DriveConstants.kMinOutput, DriveConstants.kMaxOutput);
+                m_leftPIDController.setFeedbackDevice(m_leftEncoder);
+
+                m_rightPIDController.setP(DriveConstants.kP);
+                m_rightPIDController.setI(DriveConstants.kI);
+                m_rightPIDController.setIZone(DriveConstants.kIz);
+                m_rightPIDController.setD(DriveConstants.kD);
+                m_rightPIDController.setFF(DriveConstants.kFF);
+                m_rightPIDController.setOutputRange(DriveConstants.kMinOutput, DriveConstants.kMaxOutput);
+                m_rightPIDController.setFeedbackDevice(m_rightEncoder);
+
+                //m_turnController.setTolerance(DriveConstants.kTurnTolerance);
+
+                m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d(),0,0);
+                // this is what they did in 2020 with the navX:
+                // Rotation2d.fromDegrees(getHeading()));
+                resetEncoders();
+                // from 2020: resetOdometry(new Pose2d(0, 0, new Rotation2d()));
+
+        }
+
+        public void periodic() {
+                //SmartDashboard.putNumber("the angle", getHeading());
+                // System.out.println("the angle is: " + getHeading());
+                //SmartDashboard.putNumber("average encoder", getAverageEncoderDistance());
+                // m_odometry.update(m_gyro.getRotation2d(), getLeftEncoderPosition(),
+                //                 getRightEncoderPosition());
+                 if(DriverStation.isDisabled() && m_frontLeft.getIdleMode() == IdleMode.kBrake && !DriverStation.isAutonomous()){
+                         m_frontLeft.setIdleMode(IdleMode.kCoast);
+                         m_frontRight.setIdleMode(IdleMode.kCoast);
+                 
+                 } else if(DriverStation.isEnabled()&& m_frontLeft.getIdleMode() == IdleMode.kCoast){
+                         m_frontLeft.setIdleMode(IdleMode.kBrake);
+                         m_frontRight.setIdleMode(IdleMode.kBrake);
+
+                 }
+        }
+
+        /**
+         * @return The left encoder position (meters)
+         */
+        public double getLeftEncoderPosition() {
+                return m_leftEncoder.getPosition();
+        }
+
+        /**
+         * @return The right encoder position (meters)
+         */
+        public double getRightEncoderPosition() {
+                return m_rightEncoder.getPosition();
+        }
+
+        /**
+         * @return The average encoder distance of both encoders (meters)
+         */
+        public double getAverageEncoderDistance() {
+                return (getLeftEncoderPosition() + getRightEncoderPosition()) / 2.0;
+        }
+
+        /**
+         * @return The velocity of the left encoder (meters/s)
+         */
+        public double getLeftEncoderVelocity() {
+                return m_leftEncoder.getVelocity();
+        }
+
+        /**
+         * @return The velocity of the right encoder (meters/s)
+         */
+        public double getRightEncoderVelocity() {
+                return m_rightEncoder.getVelocity();
+        }
+
+        /**
+         * @return Pose of the robot
+         */
+        public Pose2d getPose() {
+                return m_odometry.getPoseMeters();
+        }
+
+        /**
+         * @return Wheel speeds of the robot
+         */
+        public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+                return new DifferentialDriveWheelSpeeds(getLeftEncoderVelocity(), getRightEncoderVelocity());
+        }
+
+        // public double getLeftMotorSpeeds() {
+        //         return m_frontLeft.get();
+        // }
+
+        // public double getRightMotorSpeeds() {
+        //         return m_frontRight.get();
+        // }
+
+        /**
+         * @return The heading of the gyro (degrees)
+         */
+        public double getHeading() {
+                return m_gyro.getYaw() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
+        }
+
+        /**
+         * @return The rate of the gyro turn (deg/s)
+         */
+        public double getTurnRate() {
+                return m_gyro.getRate() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
+        }
+
+        // public void setTurnAngle(double angle) {
+        //         m_turnController.setSetpoint(angle);
+        // }
+
+        /**
+         * Resets gyro position to 0
+         */
+        public void zeroHeading() {
+                m_gyro.zeroYaw();
+        }
+
+        /**
+         * Sets both encoders to 0
+         */
+        public void resetEncoders() {
+                m_leftEncoder.setPosition(0);
+                m_rightEncoder.setPosition(0);
+        }
+
+        /**
+         * @param pose Pose to set the robot to
+         */
+        public void resetOdometry(Pose2d pose) {
+                resetEncoders();
+                m_odometry.resetPosition(m_gyro.getRotation2d(),0,0,pose);
+        }
+
+
+        public void arcadeDrive(double straight, double left, double right) {
+                tankDrive(DriveConstants.kSpeedLimitFactor * (straight - left + right),
+                                DriveConstants.kSpeedLimitFactor * (straight + left - right));
+        }
+
+        /**
+         * @param leftSpeed  Left motors percent output
+         * @param rightSpeed Right motors percent output
+         */
+        public void tankDrive(double leftSpeed, double rightSpeed) {
+                // System.out.println("Left speed: " + leftSpeed);
+                // System.out.println("Right speed:" + rightSpeed);
+                m_frontLeft.set(leftSpeed);
+                m_backLeft.set(leftSpeed);
+                m_frontRight.set(rightSpeed);
+                m_backRight.set(rightSpeed);
+                
+        }
+
+        
+        public void tankDriveVelocity(DifferentialDriveWheelSpeeds wheelSpeeds) {
+
+                double leftNativeVelocity = wheelSpeeds.leftMetersPerSecond
+                                * (1 / DriveConstants.kEncoderVelocityConversionFactor);
+                double rightNativeVelocity = wheelSpeeds.rightMetersPerSecond
+                                * (1 / DriveConstants.kEncoderVelocityConversionFactor);
+
+                m_leftPIDController.setReference(leftNativeVelocity, CANSparkMax.ControlType.kVelocity);
+                m_rightPIDController.setReference(rightNativeVelocity, CANSparkMax.ControlType.kVelocity);
+
+                // same as above except implementing a feed forward as well
+                // m_leftPIDController.setReference(leftNativeVelocity,
+                // CANSparkMax.ControlType.kVelocity,
+                // DriveConstants.kSlotID,
+                // DriveConstants.kFeedForward.calculate(wheelSpeeds.leftMetersPerSecond));
+        }
+}
