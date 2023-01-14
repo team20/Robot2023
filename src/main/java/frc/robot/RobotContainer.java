1d1b5ba5d35d7dd3cf4328c0b44a28c218ce309a
23watejona <58493718+23watejona@users.noreply.github.com>
2023-01-14 14:56:03 -0500

    changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,24 +4,30 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.drive.BalanceNoPIDCommand;
+import frc.robot.commands.drive.DefaultDriveCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.GripperSubsystem;
-
+import frc.robot.Constants.ControllerConstants.Axis;
 public class RobotContainer {
   private DriveSubsystem m_driveSubsystem = new DriveSubsystem();
   private ArmSubsystem m_armSubsystem = new ArmSubsystem();
   private GripperSubsystem m_gripperSubsystem = new GripperSubsystem();
 
+  private Joystick m_driverController = new Joystick(0);
   public RobotContainer() {
     configureButtonBindings();
   }
 
   private void configureButtonBindings() {
-
+    DriveSubsystem.get().setDefaultCommand(new DefaultDriveCommand(
+    () -> -m_driverController.getRawAxis(Axis.kLeftY),
+    () -> m_driverController.getRawAxis(Axis.kLeftTrigger),
+    () -> m_driverController.getRawAxis(Axis.kRightTrigger)));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/commands/drive/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/DefaultDriveCommand.java
@@ -1,32 +1,53 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.drive;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+// import frc.robot.Constants.ArduinoConstants.LEDColors;
+// import frc.robot.Constants.ArduinoConstants.LEDModes;
+// import frc.robot.commands.ArduinoCommands.UpdateLEDsCommand;
+// import frc.robot.subsystems.ArduinoSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class DefaultDriveCommand extends CommandBase {
-  /** Creates a new DefaultDriveCommand. */
-  public DefaultDriveCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+	private final Supplier<Double> m_speedStraight, m_speedLeft, m_speedRight;
+	//private final ArduinoSubsystem m_arduinoSubsystem;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+	/**
+	 * Drive using speed inputs as a percentage output of the motor
+	 * 
+	 * @param driveSubsystem The subsystem to be used
+	 * @param speedStraight  Supplier of straight speed
+	 * @param speedLeft      Supplier of left speed
+	 * @param speedRight     Supplier of right speed
+	 */
+	public DefaultDriveCommand(Supplier<Double> speedStraight, Supplier<Double> speedLeft, Supplier<Double> speedRight) {
+		//m_arduinoSubsystem = arduinoSubsystem;
+		m_speedStraight = speedStraight;
+		m_speedLeft = speedLeft;
+		m_speedRight = speedRight;
+		addRequirements(DriveSubsystem.get());
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+	/**
+	 * Update the motor outputs
+	 */
+	public void execute() {
+		double speedStraight = Math.abs(m_speedStraight.get()) > .1 ? m_speedStraight.get()
+				: 0;
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+		double speedLeft = Math.abs(m_speedLeft.get()) > 0.05 ? m_speedLeft.get() : 0;
+		double speedRight = Math.abs(m_speedRight.get()) > 0.05 ? m_speedRight.get()
+				: 0;
+
+		if (speedStraight != 0) {
+		speedLeft *= DriveConstants.kTurningMultiplier;
+		speedRight *= DriveConstants.kTurningMultiplier;
+		}
+
+		DriveSubsystem.get().arcadeDrive(speedStraight, speedLeft, speedRight);
+
+	}
 }
