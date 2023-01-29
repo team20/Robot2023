6a36d08ad904bbbcae221e4374088cc1102311da
u.andrew.h <u.andrew.h@gmail.com>
2023-01-29 12:50:51 -0500

    added PID looping to TagAlignCommand

--- src/main/java/frc/robot/commands/drive/TagAlignCommand.java
+++ src/main/java/frc/robot/commands/drive/TagAlignCommand.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.drive;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -14,6 +15,8 @@ import frc.robot.subsystems.DriveSubsystem;
 
 public class TagAlignCommand extends CommandBase {
   /** Creates a new DefaultDriveCommand. */
+  PIDController m_controller = new PIDController(0.14, 0.005, 0.000); 
+  //i: 0.005 before d: 0.000 before
   public TagAlignCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(DriveSubsystem.get());
@@ -22,13 +25,15 @@ public class TagAlignCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_controller.setSetpoint(-1);
   }
     
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    DriveSubsystem.get().tankDrive(-0.25, -0.25);
+    //DriveSubsystem.get().tankDrive(-0.25, -0.25);
+    double speed = m_controller.calculate(AprilTagSubsystem.get().getDistance());
+    DriveSubsystem.get().tankDrive(-speed, -speed);
   }
 
   // Called once the command ends or is interrupted.
@@ -40,6 +45,6 @@ public class TagAlignCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return AprilTagSubsystem.get().getDistance() > -1.0;
+    return m_controller.atSetpoint();
   }
 }
