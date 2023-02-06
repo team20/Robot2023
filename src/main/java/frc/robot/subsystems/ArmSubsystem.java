ab97720cf28e5dcbc0487a90be1b4f14642594de
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-02-05 20:37:46 -0500

    Cleanup ArmSubsystem
    Removed old comments and offset code, documented methods, commented logging code, and changed names of logged values

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,20 +9,15 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.SparkMaxPIDController;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.util.ForwardKinematicsTool;
 import frc.robot.util.InverseKinematicsTool;
 
-// import shuffleboard logging later //;
-
 public class ArmSubsystem extends SubsystemBase {
-	/** Creates a new ArmSubsystem. :] */
+	/** Stores the instance of the ArmSubsystem */
 	private static ArmSubsystem s_subsystem;
-
-	// -------------------adjust encoders/pidcontrollers for 1 and 2 (& possibly 3)
 	private final CANSparkMax m_lowerArmMotor = new CANSparkMax(ArmConstants.kLowerMotor, MotorType.kBrushless);
 	private final CANSparkMax m_upperArmMotor = new CANSparkMax(ArmConstants.kUpperMotor, MotorType.kBrushless);
 
@@ -30,29 +25,24 @@ public class ArmSubsystem extends SubsystemBase {
 			.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
 	private final SparkMaxAbsoluteEncoder m_upperArmEncoder = m_upperArmMotor
 			.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
+
 	private final SparkMaxPIDController m_lowerArmController = m_lowerArmMotor.getPIDController();
 	private final SparkMaxPIDController m_upperArmController = m_upperArmMotor.getPIDController();
-	private double m_xOffset = 0;
-	private double m_yOffset = 0;
 
 	/**
-	 * Initializes a new instance of the {@link ArmSubsystem} class.
+	 * Instantiate a new instance of the {@link ArmSubsystem} class.
 	 */
-
-	// so i'm assuming there needs to be twice of what's below: for both
-	// m_lowerArmMotor
-	// and m_motor2 (joints... 1&2)
 	public ArmSubsystem() {
 		// Singleton
 		if (s_subsystem != null) {
 			try {
-				throw new Exception("Arm subsystem already initalized!");
+				throw new Exception("Arm subsystem already initialized!");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		s_subsystem = this;
-
+		// Initialize lower arm
 		m_lowerArmMotor.restoreFactoryDefaults();
 		m_lowerArmMotor.setInverted(ArmConstants.kInvert);
 		m_lowerArmMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
@@ -72,6 +62,7 @@ public class ArmSubsystem extends SubsystemBase {
 		m_lowerArmController.setPositionPIDWrappingMaxInput(360);
 		m_lowerArmController.setPositionPIDWrappingMinInput(0);
 
+		// Initialize upper arm
 		m_upperArmMotor.restoreFactoryDefaults();
 		m_upperArmMotor.setInverted(ArmConstants.kInvert);
 		m_upperArmMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
@@ -79,11 +70,7 @@ public class ArmSubsystem extends SubsystemBase {
 		m_upperArmMotor.setSmartCurrentLimit(ArmConstants.kSmartCurrentLimit);
 		m_upperArmEncoder.setPositionConversionFactor(360);
 		m_upperArmEncoder.setZeroOffset(ArmConstants.kUpperEncoderZeroOffset);
-		// now for second motor?:
-		// m_motor.setSecondaryCurrentLimit(ArmConstants.kPeakCurrentLimit,
-		// ArmConstants.kPeakCurrentDurationMillis);
 
-		// pidController 3:
 		m_upperArmController.setP(ArmConstants.kP);
 		m_upperArmController.setI(ArmConstants.kI);
 		m_upperArmController.setIZone(ArmConstants.kIz);
@@ -108,30 +95,6 @@ public class ArmSubsystem extends SubsystemBase {
 		// ArmConstants.kSlotID);
 	}
 
-	public double getXOffset() {
-		return m_xOffset;
-	}
-
-	public double getYOffset() {
-		return m_yOffset;
-	}
-
-	public void setXOffset(double xOffset) {
-		m_xOffset = xOffset;
-	}
-
-	public void setYOffset(double yOffset) {
-		m_yOffset = yOffset;
-	}
-
-	public void changeXOffset(double xOffset) {
-		m_xOffset += xOffset;
-	}
-
-	public void changeYOffset(double yOffset) {
-		m_yOffset += yOffset;
-	}
-
 	public static ArmSubsystem get() {
 		return s_subsystem;
 	}
@@ -144,38 +107,58 @@ public class ArmSubsystem extends SubsystemBase {
 		m_lowerArmMotor.set(speed);
 	}
 
+	/**
+	 * @return
+	 *         The angle of the lower arm in degrees
+	 */
 	public double getLowerArmPosition() {
 		return m_lowerArmEncoder.getPosition();
 	}
 
+	/**
+	 * @return
+	 *         The angle of the upper arm in degrees
+	 */
 	public double getUpperArmPosition() {
 		return m_upperArmEncoder.getPosition();
 	}
 
+	/**
+	 * Sets the angle for the lower arm, and logs the angle
+	 * 
+	 * @param angle
+	 *              The target angle of the lower arm in degrees
+	 */
 	public void setLowerArmPosition(double angle) {
 		m_lowerArmController.setReference(angle, ControlType.kPosition);
 		SmartDashboard.putNumber("Target Lower Arm Angle", angle);
 	}
 
+	/**
+	 * Sets the angle for the upper arm, and logs the angle
+	 * 
+	 * @param angle
+	 *              The target angle of the upper arm in degrees
+	 */
 	public void setUpperArmPosition(double angle) {
 		m_upperArmController.setReference(angle, ControlType.kPosition);
 		SmartDashboard.putNumber("Target Upper Arm Angle", angle);
 	}
 
+	// This method will be called once per scheduler run
 	@Override
 	public void periodic() {
-		// This method will be called once per scheduler run
+		// Log the lower and upper arm angle as measured by the encoders
 		SmartDashboard.putNumber("Current Lower Arm Angle", getLowerArmPosition());
 		SmartDashboard.putNumber("Current Upper Arm Angle", getUpperArmPosition());
-
+		// Calculate the arm position using the encoder angles
 		double[] coordinates = ForwardKinematicsTool.getArmPosition(getUpperArmPosition(), getLowerArmPosition());
 		SmartDashboard.putNumber("Forward X", coordinates[0]);
 		SmartDashboard.putNumber("Forward Y", coordinates[1]);
-
+		// Take the calculated arm position from the forward kinematics code, and
+		// calculate the lower and upper arm angles to make sure everything works
 		Double[] armPosition = InverseKinematicsTool.getArmAngles(coordinates[0], coordinates[1]);
-		SmartDashboard.putNumber("inverse angle lower", armPosition[0]);
-		SmartDashboard.putNumber("inverse angle upper", armPosition[1]);
-		// SmartDashboard.putNumber("Raw Upper Arm position", )
-		// System.out.println(m_upperArmController.getP());
+		SmartDashboard.putNumber("IK Lower Arm Angle", armPosition[0]);
+		SmartDashboard.putNumber("IK Upper Arm Angle", armPosition[1]);
 	}
 }
