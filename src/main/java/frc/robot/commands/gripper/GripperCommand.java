438d1f935999a8e7823ca0b0723386cfd6c6b4e0
anvita-n <122415279+anvita-n@users.noreply.github.com>
2023-02-17 20:50:03 -0500

    Update GripperCommand.java

--- src/main/java/frc/robot/commands/gripper/GripperCommand.java
+++ src/main/java/frc/robot/commands/gripper/GripperCommand.java
@@ -1,19 +1,24 @@
 package frc.robot.commands.gripper;
 
+import java.time.LocalDateTime;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.GripperConstants;
 import frc.robot.subsystems.GripperSubsystem;
 
 public class GripperCommand extends CommandBase {
     public enum GripperPosition {
-        CLOSECUBE,
-        CLOSECONE,
-        OPEN
+        //CLOSECUBE,
+        CLOSE,
+        OPEN,
+        ZERO
     }
 
     private GripperPosition m_gripperPosition;
+    private long  m_startTime = 0;
 
     public GripperCommand(GripperPosition gripperPosition) {
-        m_gripperPosition = gripperPosition;
+        m_gripperPosition = gripperPosition; 
         addRequirements(GripperSubsystem.get());
     }
 
@@ -26,23 +31,14 @@ public class GripperCommand extends CommandBase {
     public void execute() {
         switch (m_gripperPosition) {
             case OPEN:
-                while (GripperSubsystem.get().getOpenLimitSwitch() == false) {
-                    // while limit switch is not pressed, motor will run
-                    GripperSubsystem.get().setGripperMotor(0.1);
-                }
-                // When it is pressed, stop
-                GripperSubsystem.get().setGripperMotor(0);
+                GripperSubsystem.get().setGripperPosition(GripperConstants.kGripperOpenPosition);
                 break;
             // To do: limit switch stuff for cube vs cone
-            case CLOSECONE:
-                while (GripperSubsystem.get().getCloseLimitSwitch() == false) {
-                    // while limit switch is not pressed, motor will run
-                    GripperSubsystem.get().setGripperMotor(0.1);
-                }
-                // When it is pressed, stop
-                GripperSubsystem.get().setGripperMotor(0);
+            case CLOSE:
+                GripperSubsystem.get().setGripperMotor(.1);
+                if (m_startTime == 0) {m_startTime = System.currentTimeMillis()}
                 break;
-            case CLOSECUBE:
+            case ZERO:
                 while (GripperSubsystem.get().getCloseLimitSwitch() == false) {
                     // While limit switch is not pressed, motor will run
                     GripperSubsystem.get().setGripperMotor(-0.1);
@@ -62,7 +58,35 @@ public class GripperCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return false;
+        switch (m_gripperPosition) {
+            case OPEN:
+                if (Math.abs(GripperSubsystem.get().getGripperEncoderPosition()-GripperConstants.kGripperOpenPosition) < 10)
+                     GripperSubsystem.get().setGripperMotor(0);
+                     return true; 
+                    }
+             break;
+        
+
+            case CLOSE:
+                    if (System.currentTimeMillis()-m_startTime > GripperConstants.kCloseTime) {
+                        GripperSubsystem.get().setGripperMotor(GripperConstants.kHoldPower);
+                        return true;
+                    }
+            break;
+            case ZERO:
+                if (System.currentTimeMillis()-m_startTime > GripperConstants.kCloseTime) {
+                    GripperSubsystem.get().setGripperMotor(0);
+                    GripperSubsystem.get().resetZero();
+                    return true;
+            }
+            break;
+            default:
+            break;
+
+
+
+        }
+
     }
 
 }
