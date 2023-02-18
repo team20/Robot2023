d7fe422792591ff2002990d7ee0ee193731d65ee
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-02-18 10:55:32 -0500

    Revert "Merge remote-tracking branch 'origin/stage' into arm-prototype"
    This reverts commit 04326f3423210035551002fb3f6e3b65728ff543, reversing
    changes made to 676ebc57ea757ea42f6dea1b79bb4d4b5b161768.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,45 +4,18 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.ArduinoConstants;
-import frc.robot.subsystems.ArduinoSubsystem;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.ControllerConstants;
-import frc.robot.commands.arm.ArmScoreCommand;
-import frc.robot.commands.arm.UpCommand;
-import frc.robot.commands.arm.ArmScoreCommand.ArmPosition;
-import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.GripperSubsystem;
 
 public class RobotContainer {
-	private DriveSubsystem m_driveSubsystem = new DriveSubsystem();
-	private ArmSubsystem m_armSubsystem = new ArmSubsystem();
-	private GripperSubsystem m_gripperSubsystem = new GripperSubsystem();
-	private ArduinoSubsystem m_arduinoSubsystem = new ArduinoSubsystem();
-	private final GenericHID m_controller = new GenericHID(ControllerConstants.kDriverControllerPort);
 
 	public RobotContainer() {
 		configureButtonBindings();
 	}
 
 	private void configureButtonBindings() {
-		// m_armSubsystem.setDefaultCommand(new
-		// ArmCommand(ArmCommand.Operation.CMD_ARM_DOWN));
-		new Trigger(() -> m_controller.getRawButton(ControllerConstants.Button.kTriangle))
-				.onTrue(new ArmScoreCommand(ArmPosition.HIGH));
-		new Trigger(() -> m_controller.getRawButton(ControllerConstants.Button.kSquare))
-				.onTrue(new ArmScoreCommand(ArmPosition.LOW));
-		new Trigger(() -> m_controller.getRawButton(ControllerConstants.Button.kCircle))
-				.onTrue(new ArmScoreCommand(ArmPosition.MEDIUM));
-		// new Trigger(() ->
-		// m_controller.getRawButton(ControllerConstants.Button.kTriangle))
-		// .onTrue(new UpCommand());
 	}
 
 	public Command getAutonomousCommand() {
-		return new ArmScoreCommand(ArmPosition.LOW);
+		return null;
 	}
 }
