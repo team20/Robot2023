25c67fe5785c3ff1fc0137bb67fcb7a7a33a1770
23watejona <58493718+23watejona@users.noreply.github.com>
2023-03-19 12:10:49 -0400

    Update ArmScoreCommand.java

--- src/main/java/frc/robot/commands/arm/ArmScoreCommand.java
+++ src/main/java/frc/robot/commands/arm/ArmScoreCommand.java
@@ -20,14 +20,13 @@ public class ArmScoreCommand extends CommandBase {
 		MEDIUM_BACK,
 		LOW,
 		POCKET,
-		SUBSTATION,
 		TO_BACK_INTERMEDIATE,
 		TO_FORWARD_INTERMEDIATE,
 		HIGH_INTERMEDIATE,
 		POCKET_INTERMEDIATE,
 		SETTLE_POSITION,
-		/** Exists to forcibly finish this command */
-		HOLD
+		HOLD,
+		SUBSTATION
 	}
 
 	/** Stores the position we want the arm to move to */
@@ -64,8 +63,8 @@ public class ArmScoreCommand extends CommandBase {
 			case POCKET:
 				angles = ArmConstants.kPocketAngles;
 				break;
-			case SUBSTATION:
-				angles = ArmConstants.kSubstationAngles;
+			case POCKET_INTERMEDIATE:
+				angles = ArmConstants.kPocketIntermediateAngles;
 				break;
 			case TO_BACK_INTERMEDIATE:
 				angles = ArmConstants.kToBackIntermediateAngles;
@@ -75,9 +74,6 @@ public class ArmScoreCommand extends CommandBase {
 				break;
 			case HIGH_INTERMEDIATE:
 				angles = ArmConstants.kHighIntermediateAngles;
-				break;
-			case POCKET_INTERMEDIATE:
-				angles = ArmConstants.kPocketIntermediateAngles;
 				break;
 			case HOLD:
 				angles = new double[2];
