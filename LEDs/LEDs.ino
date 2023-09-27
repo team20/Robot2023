3ff7dc70c7c840de6db07627fab060cc2fc13335
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-09-26 23:16:51 -0400

    Cleanup

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -3,10 +3,6 @@
 #ifdef __AVR__
 #include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
-
-#define ITERATION_DELAY_MS 10
-#define NAVX_SENSOR_DEVICE_I2C_ADDRESS_7BIT 0x32
-#define NUM_BYTES_TO_READ 1
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1:
 #define UPPER_LED_PIN 5
@@ -30,21 +26,22 @@ Adafruit_NeoPixel lowerStrip(LED_COUNT, LOWER_LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-int colorIndex = 0;  // frame variable, changes from loop
-int pattern = 10;    // pattern led strips are on, read in from master/robot
-long startTime = -1;
-const long endTime = 30000;                        // timer variables for endgame
-uint32_t teamColor = upperStrip.Color(0, 255, 0);  // color of team, default to green, can be set by master/robot to alliance color
+// frame variable, changes from loop
+int colorIndex = 0;
+// pattern led strips are on, read in from master/robot
+int pattern = 10;
+// color of team, default to green
+uint32_t teamColor = color(0, 255, 0);
 
 /// @brief An array of 6 colors in rainbow order(ROY G BV)
 uint32_t RainbowColor[] = {
-    upperStrip.Color(255, 0, 0),
-    upperStrip.Color(255, 165, 0),
-    upperStrip.Color(255, 255, 0),
-    upperStrip.Color(0, 255, 0),
-    upperStrip.Color(0, 0, 255),
-    upperStrip.Color(148, 0, 211)};
-// setup() function -- runs once at startup --------------------------------
+    color(255, 0, 0),
+    color(255, 165, 0),
+    color(255, 255, 0),
+    color(0, 255, 0),
+    color(0, 0, 255),
+    color(148, 0, 211)};
+
 void setup() {
 	// These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
 	// Any other board, you can remove this part (but no harm leaving it):
@@ -62,86 +59,89 @@ void setup() {
 	Wire.begin(0x18);
 	Wire.onReceive(receiveEvent);
 }
-void loop() {
-	// if (Serial.available() > 0) {
-	// 	pattern = Serial.read();
-	// 	for (int i = 0; i < LED_COUNT; i++) {
-	// 		upperStrip.setPixelColor(i, lowerStrip.Color(0, 255, 0));
-	// 	}
-	// } else {
-	// 	for (int i = 0; i < LED_COUNT; i++) {
-	// 		upperStrip.setPixelColor(i, lowerStrip.Color(255, 0, 0));
-	// 	}
-	// }
 
+void loop() {
 	switch (pattern) {  // sets up lights to patterns
 		                // note: every function returns a color based on colorIndex, the pixel index, and optional color parameters.
-		                // the for loops set the pixels to have their corrseponding colors based on the pattern function on the colorIndex frame
+		                // the for loops set the pixels to have their corresponding colors based on the pattern function on the colorIndex frame
 		case 8:         // reset code
-			startTime = -1;
 			colorIndex = 0;
 			pattern = -1;
 		case 9:  // blinking yellow
 			for (int i = 0; i < LED_COUNT; i++) {
-				upperStrip.setPixelColor(i, BlinkingLights(colorIndex, upperStrip.Color(245, 149, 24), upperStrip.Color(0, 0, 0)));
-				lowerStrip.setPixelColor(i, BlinkingLights(colorIndex, lowerStrip.Color(245, 149, 24), lowerStrip.Color(0, 0, 0)));
+				upperStrip.setPixelColor(i, BlinkingLights(colorIndex, color(245, 149, 24), color(0, 0, 0)));
+				lowerStrip.setPixelColor(i, BlinkingLights(colorIndex, color(245, 149, 24), color(0, 0, 0)));
 			}
 			break;
 		case 10:  // blinking purple
 			for (int i = 0; i < LED_COUNT; i++) {
-				upperStrip.setPixelColor(i, BlinkingLights(colorIndex, upperStrip.Color(230, 0, 255), upperStrip.Color(0, 0, 0)));
-				lowerStrip.setPixelColor(i, BlinkingLights(colorIndex, lowerStrip.Color(230, 0, 255), lowerStrip.Color(0, 0, 0)));
+				upperStrip.setPixelColor(i, BlinkingLights(colorIndex, color(230, 0, 255), color(0, 0, 0)));
+				lowerStrip.setPixelColor(i, BlinkingLights(colorIndex, color(230, 0, 255), color(0, 0, 0)));
 			}
 			break;
 		case 16:
 			for (int i = 0; i < LED_COUNT; i++) {
 				upperStrip.setPixelColor(i, RainbowPartyFunTime(colorIndex, i));
 				lowerStrip.setPixelColor(i, RainbowPartyFunTime(colorIndex, i));
-				// Serial.println("11");
 			}
 			break;
 		case 17:
 			for (int i = 0; i < LED_COUNT; i++) {
 				upperStrip.setPixelColor(i, SuperRainbowPartyFunTime(colorIndex));
+				lowerStrip.setPixelColor(i, SuperRainbowPartyFunTime(colorIndex));
 			}
 			break;
-		case -1:
-			for (int i = 0; i < LED_COUNT; i++) {
-				upperStrip.setPixelColor(i, upperStrip.Color(255, 0, 0));
-			}
-			break;
-		default:  // display team/alliance color
+		default:  // display team color
 			for (int i = 0; i < LED_COUNT; i++) {
 				upperStrip.setPixelColor(i, teamColor);
 				lowerStrip.setPixelColor(i, teamColor);
 			}
 			break;
 	}
-	upperStrip.show();  // show
+	upperStrip.show();
 	lowerStrip.show();
 	colorIndex++;  // next frame
 	delay(100);
 }
+
 void receiveEvent(int bytes) {
 	byte x = Wire.read();
 	pattern = x;
 }
+
 void serialEvent() {
 	pattern = Serial.read();
 }
 
-uint32_t SuperRainbowPartyFunTime(int c) {
-	return RainbowColor[c % 6];
+/**
+ * @brief Makes LEDs cycle through the colors of the rainbow
+ * @param x The number to control the color. Use colorIndex
+ * to have the entire strip cycle through the rainbow, or LED number
+ * to have the individual LEDs be different colors of the rainbow
+ * @return The color
+ */
+uint32_t SuperRainbowPartyFunTime(int x) {
+	return RainbowColor[x % 6];
 }
+
+/**
+ * @brief Makes the LED strip display all the colors of the rainbow while the rainbow moves
+ * @param c The colorIndex
+ * @param i LED number
+ * @return The color
+ */
 uint32_t RainbowPartyFunTime(int c, int i) {
 	return RainbowColor[(c + i) % 6];
 }
-/// @brief Makes LEDs switch back and forth between two colors
-/// @param c Color index
-/// @param i LED number
-/// @param color1 A color
-/// @param color2 The other color
-/// @return The color that the LED should show
+
+/**
+ * @brief Makes individual LEDs switch back and forth between two colors
+ * @param c Color index
+ * @param i LED number
+ * @param color1 A color
+ * @param color2 The other color
+ * @return The color that the LED should show
+ */
 uint32_t TheaterLights(int c, int i, uint32_t color1, uint32_t color2) {  // pixel on color 1 or 2 depending on frame
 	if (i % 2 == c % 2) {
 		return color1;
@@ -149,24 +149,22 @@ uint32_t TheaterLights(int c, int i, uint32_t color1, uint32_t color2) {  // pix
 	return color2;
 }
 
-/// @brief Makes LEDs switch back and forth between colors,
-/// using color index will make the whole strip alternate colors,
-/// using LED number will make even numbered LEDs one color, and odd numbered LEDs another color
-/// @param x A number determining the color to use
-/// @param color3 A color
-/// @param color4 The other color
-/// @return The color for the LED
+/**
+ * @brief Makes LEDs switch back and forth between colors,
+ * using color index will make the whole strip alternate colors,
+ * using LED number will make even numbered LEDs one color, and odd numbered LEDs another color
+ * @param x A number determining the color to use
+ * @param color3 A color
+ * @param color4 The other color
+ * @return The color for the LED
+ */
 uint32_t BlinkingLights(int x, uint32_t color3, uint32_t color4) {
 	if (x % 2 == 0) {
 		return color3;
 	}
 	return color4;
 }
-uint32_t Timer(int c, int i, uint32_t color) {  // user timer proportion to light up specific pixel
-	if (c % 2 == 0 &&
-	    2 > (i + c) % int(LED_COUNT * (1 - (millis() - startTime) / endTime)) &&
-	    i * endTime > LED_COUNT * (millis() - startTime)) {
-		return color;
-	}
-	return upperStrip.Color(0, 0, 0);
+
+uint32_t color(int r, int g, int b) {
+	return Adafruit_NeoPixel::Color(r, g, b);
 }
