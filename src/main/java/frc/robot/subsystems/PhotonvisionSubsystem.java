29f6b91e2cc0fc582008da6a483b40a270afc987
Hwang1907 <69113243+Hwang1907@users.noreply.github.com>
2023-11-29 22:08:25 -0500

    cleaned up code a little bit

--- src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
@@ -11,9 +11,10 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /**
- * Operates the Robot's ability to detect AprilTags using Photonvision
+ *  Operates the Robot's ability to detect AprilTags using Photonvision
+ *  @author Andrew Hwang
  */
-public class PhotonvisionSubsystem extends SubsystemBase{
+public class PhotonvisionSubsystem extends SubsystemBase {
 
     // Member Variables
 	public double m_x, m_y, m_z, m_pitch, m_yaw, m_qw, m_qx, m_qy, m_qz;
@@ -23,21 +24,12 @@ public class PhotonvisionSubsystem extends SubsystemBase{
 
 
     // Median Filters
-    private MedianFilter m_filterX = new MedianFilter(10);
-	private MedianFilter m_filterY = new MedianFilter(10);
-	private MedianFilter m_filterZ = new MedianFilter(10);
-	private MedianFilter m_filterPitch = new MedianFilter(10);
-	private MedianFilter m_filterYaw = new MedianFilter(10);
-	private MedianFilter m_filterQW = new MedianFilter(10);
-	private MedianFilter m_filterQX = new MedianFilter(10);
-	private MedianFilter m_filterQY = new MedianFilter(10);
-	private MedianFilter m_filterQZ = new MedianFilter(10);
-	private MedianFilter m_filterRollT = new MedianFilter(10);
-	private MedianFilter m_filterYawT = new MedianFilter(10);
+    private MedianFilter m_filterX, m_filterY, m_filterZ, m_filterPitch, m_filterYaw, 
+    m_filterQW, m_filterQX, m_filterQY, m_filterQZ = new MedianFilter(10);
 
-
-    public PhotonvisionSubsystem() { // constructor, makes the apriltagSubsystem = to the first instance called
-		// Singleton
+    // Constructor
+    public PhotonvisionSubsystem() {
+        // Singleton
 		if (s_subsystem != null) {
 			try {
 				throw new Exception("Photonvision subsystem already initialized!");
@@ -47,22 +39,29 @@ public class PhotonvisionSubsystem extends SubsystemBase{
 		}
 		s_subsystem = this;
 	}
+
     public static PhotonvisionSubsystem get() {
         return s_subsystem;
     }
 
-
+    /**
+     * A method run twenty times per second.
+     */
     @Override
     public void periodic() {
-        // targetpose contains 7 values
+        // targetPose contains 7 values
         // The first three are x, y, and z values of the target relative to the robot
         // The last four values are represented by a quaternion: qw, qx, qy, qz
         // qw is a scalar multplied to a three tuple vector [qx, qy, qz]
         NetworkTableEntry robottran = photonTable.getEntry("targetPose");
+
+        // targetPitch and targetYaw receives the pitch and the yaw of the robot relative to the tag
         NetworkTableEntry pitchEntry = photonTable.getEntry("targetPitch");
         NetworkTableEntry yawEntry = photonTable.getEntry("targetYaw");
 
         double[] targetRelative = robottran.getDoubleArray(new double[6]);
+
+        // If a tag is in view of the camera and is recording data onto the Network Tables
         if (!targetRelative.equals(new double[6])) {
             m_x = m_filterX.calculate(targetRelative[0]);
             m_y = m_filterY.calculate(targetRelative[1]);
@@ -78,17 +77,52 @@ public class PhotonvisionSubsystem extends SubsystemBase{
             m_tagInView = false;
         }
 
+        // Data is updated onto SmartDashboard
         SmartDashboard.putNumber("Photonvision X", m_x);
 		SmartDashboard.putNumber("Photonvision Y", m_y);
 		SmartDashboard.putNumber("Photonvision Z", m_z);
-		SmartDashboard.putNumber("Photonvision Pitch", m_pitch);
-		SmartDashboard.putNumber("Photonvision Yaw", m_yaw);
-		SmartDashboard.putBoolean("Tag in View", m_tagInView);
         SmartDashboard.putNumber("Photonvision QW", m_qw);
         SmartDashboard.putNumber("Photonvision QX", m_qx);
 		SmartDashboard.putNumber("Photonvision QY", m_qy);
 		SmartDashboard.putNumber("Photonvision QZ", m_qz);
-
+		SmartDashboard.putNumber("Photonvision Pitch", m_pitch);
+		SmartDashboard.putNumber("Photonvision Yaw", m_yaw);
+		SmartDashboard.putBoolean("Tag in View", m_tagInView);
+        
     }
-    // TODO: add getter methods
+    /**
+	 * Returns the distance between the camera and the AprilTag
+	 * 
+	 * @return the distance between the camera and the AprilTag
+	 */
+	public double getDistance() {
+		return m_z;
+	}
+
+	/**
+	 * Returns the yaw angle between the camera and the AprilTag
+	 * 
+	 * @return the yaw angle between the camera and the AprilTag
+	 */
+	public double getYaw() {
+		return m_yaw;
+	}
+
+	/**
+	 * Returns the x (horizontal) translation from the AprilTag
+	 * 
+	 * @return the x (horizontal) translation from the AprilTag
+	 */
+	public double getX() {
+		return m_x;
+	}
+
+	/**
+	 * Returns a boolean statement if the AprilTag is within the view of the camera.
+	 * 
+	 * @return a boolean statement if the AprilTag is within the view of the camera.
+	 */
+	public boolean tagInView() {
+		return m_tagInView;
+	}
 }
