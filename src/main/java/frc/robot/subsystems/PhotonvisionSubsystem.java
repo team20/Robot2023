8734ce6273ec3277c0e5f082a36c7fba125bebf8
Hwang1907 <69113243+Hwang1907@users.noreply.github.com>
2023-11-30 17:20:07 -0500

    Cleaned up code even more

--- src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonvisionSubsystem.java
@@ -4,9 +4,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,15 +15,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class PhotonvisionSubsystem extends SubsystemBase {
 
     // Member Variables
-	public double m_x, m_y, m_z, m_pitch, m_yaw, m_qw, m_qx, m_qy, m_qz;
-    NetworkTable photonTable = NetworkTableInstance.getDefault().getTable("photonvision");
+	public double m_latencyMillis;
+    public double[] m_targetPose;
+    NetworkTable photonTable = NetworkTableInstance.getDefault().getTable("photonvision/MAIN");
     private static PhotonvisionSubsystem s_subsystem;
-    public boolean m_tagInView;
-
-
-    // Median Filters
-    private MedianFilter m_filterX, m_filterY, m_filterZ, m_filterPitch, m_filterYaw, 
-    m_filterQW, m_filterQX, m_filterQY, m_filterQZ = new MedianFilter(10);
 
     // Constructor
     public PhotonvisionSubsystem() {
@@ -49,54 +42,18 @@ public class PhotonvisionSubsystem extends SubsystemBase {
      */
     @Override
     public void periodic() {
-        // targetPose contains 7 values
-        // The first three are x, y, and z values of the target relative to the robot
-        // The last four values are represented by a quaternion: qw, qx, qy, qz
-        // qw is a scalar multplied to a three tuple vector [qx, qy, qz]
-        NetworkTableEntry robottran = photonTable.getEntry("targetPose");
-
-        // targetPitch and targetYaw receives the pitch and the yaw of the robot relative to the tag
-        NetworkTableEntry pitchEntry = photonTable.getEntry("targetPitch");
-        NetworkTableEntry yawEntry = photonTable.getEntry("targetYaw");
-
-        double[] targetRelative = robottran.getDoubleArray(new double[6]);
-
-        // If a tag is in view of the camera and is recording data onto the Network Tables
-        if (!targetRelative.equals(new double[6])) {
-            m_x = m_filterX.calculate(targetRelative[0]);
-            m_y = m_filterY.calculate(targetRelative[1]);
-            m_z = m_filterZ.calculate(targetRelative[2]);
-            m_qw = m_filterQW.calculate(targetRelative[3]);
-            m_qx = m_filterQX.calculate(targetRelative[4]);
-            m_qy = m_filterQY.calculate(targetRelative[5]);
-            m_qz = m_filterQZ.calculate(targetRelative[6]);
-            m_pitch = m_filterPitch.calculate(pitchEntry.getDouble(0));
-            m_yaw = m_filterYaw.calculate(yawEntry.getDouble(0));
-            m_tagInView = true;
-        } else {
-            m_tagInView = false;
-        }
-
-        // Data is updated onto SmartDashboard
-        SmartDashboard.putNumber("Photonvision X", m_x);
-		SmartDashboard.putNumber("Photonvision Y", m_y);
-		SmartDashboard.putNumber("Photonvision Z", m_z);
-        SmartDashboard.putNumber("Photonvision QW", m_qw);
-        SmartDashboard.putNumber("Photonvision QX", m_qx);
-		SmartDashboard.putNumber("Photonvision QY", m_qy);
-		SmartDashboard.putNumber("Photonvision QZ", m_qz);
-		SmartDashboard.putNumber("Photonvision Pitch", m_pitch);
-		SmartDashboard.putNumber("Photonvision Yaw", m_yaw);
-		SmartDashboard.putBoolean("Tag in View", m_tagInView);
-        
+        m_latencyMillis = photonTable.getEntry("latencyMillis").getDouble(0.0);
+        m_targetPose = photonTable.getEntry("targetPose").getDoubleArray(new double[7]);
+        SmartDashboard.putNumber("latencyMillis", m_latencyMillis);
+		SmartDashboard.putNumberArray("targetPose", m_targetPose);
     }
     /**
 	 * Returns the distance between the camera and the AprilTag
 	 * 
 	 * @return the distance between the camera and the AprilTag
 	 */
-	public double getDistance() {
-		return m_z;
+	public double getLatencyMillis() {
+		return m_latencyMillis;
 	}
 
 	/**
@@ -104,25 +61,8 @@ public class PhotonvisionSubsystem extends SubsystemBase {
 	 * 
 	 * @return the yaw angle between the camera and the AprilTag
 	 */
-	public double getYaw() {
-		return m_yaw;
+	public double[] getTargetPose() {
+		return m_targetPose;
 	}
 
-	/**
-	 * Returns the x (horizontal) translation from the AprilTag
-	 * 
-	 * @return the x (horizontal) translation from the AprilTag
-	 */
-	public double getX() {
-		return m_x;
-	}
-
-	/**
-	 * Returns a boolean statement if the AprilTag is within the view of the camera.
-	 * 
-	 * @return a boolean statement if the AprilTag is within the view of the camera.
-	 */
-	public boolean tagInView() {
-		return m_tagInView;
-	}
 }
